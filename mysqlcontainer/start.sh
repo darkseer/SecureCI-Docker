61c2299e327ef1d274bcfea9f1145663e6cce7a2
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-01-22 21:49:20 -0500

    added wait to ensure db is up
    Former-commit-id: 6386ebe46c9572535d720255a07d7f7d7d883b64

--- mysqlcontainer/start.sh
+++ mysqlcontainer/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Start maria
 /usr/bin/mysqld_safe --basedir=/usr &
+
+# Wait until DB is up
+until mysqladmin ping >/dev/null 2>&1; do
+  echo -n "."; sleep 0.2
+done
+
+# Init DB and first user
 mysql mysql < init.sql
+
+# put db in forground to hang process
 fg
