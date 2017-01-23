f62d83c655afb1e227ee19337b9b8bd00ea9ac2e
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-01-22 23:57:31 -0500

    add properties to tomcat if they exist
    Former-commit-id: a9573ea813ef1455bedd00a94992e114d12a93aa

--- tomcatcontainer/start.sh
+++ tomcatcontainer/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.properties /opt/tomcat9/lib; fi
 cp /home/tomcat/tmp/*.war /opt/tomcat9/webapps
 /opt/tomcat9/bin/catalina.sh run
