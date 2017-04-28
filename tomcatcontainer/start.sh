691e4346d15b674353f77f3d4bf69e657c6876e5
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-04-27 22:57:40 -0400

    Get the properties files into tomcat

--- tomcatcontainer/start.sh
+++ tomcatcontainer/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.properties /opt/tomcat9/lib; fi
 cp /home/tomcat/tmp/*.war /opt/tomcat9/webapps
+cp /home/tomcat/tmp/*.properties /opt/tomcat9/conf
 /opt/tomcat9/bin/catalina.sh run
