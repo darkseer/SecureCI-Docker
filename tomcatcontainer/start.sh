8b23905270a63506e6344bced3eced2cfe9617ff
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-04-28 22:56:52 -0400

    may need an extra newline

--- tomcatcontainer/start.sh
+++ tomcatcontainer/start.sh
@@ -2,4 +2,5 @@
 if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.properties /opt/tomcat9/conf; fi
 cp /home/tomcat/tmp/*.war /opt/tomcat9/webapps
 echo ${MYSQL_PORT} | sed -e 's/tcp:\(\/\/.*\)/db.url=jdbc:mysql:\1\/speaker/g' >> /opt/tomcat9/conf/env.properties
+echo "" >> /opt/tomcat9/conf/env.properties
 /opt/tomcat9/bin/catalina.sh run
