619c64dc48e3cba020aff715cd0c625c27855ef9
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-04-28 23:13:17 -0400

    lib not conf

--- tomcatcontainer/start.sh
+++ tomcatcontainer/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.properties /opt/tomcat9/conf; fi
+if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.properties /opt/tomcat9/lib; fi
 cp /home/tomcat/tmp/*.war /opt/tomcat9/webapps
-echo ${MYSQL_PORT} | sed -e 's/tcp:\(\/\/.*\)/db.url=jdbc:mysql:\1\/speaker/g' >> /opt/tomcat9/conf/env.properties
-echo "" >> /opt/tomcat9/conf/env.properties
+echo ${MYSQL_PORT} | sed -e 's/tcp:\(\/\/.*\)/db.url=jdbc:mysql:\1\/speaker/g' >> /opt/tomcat9/lib/env.properties
+echo "" >> /opt/tomcat9/lib/env.properties
 /opt/tomcat9/bin/catalina.sh run
