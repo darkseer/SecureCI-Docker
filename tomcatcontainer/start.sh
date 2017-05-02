986e0ea71c18d4fa6d90a69b06ea17dd83c91aa0
Glenn Buckholz <glenn.buckholz@coveros.com>
2017-05-01 23:55:26 -0400

    proper name for the it coverage test results

--- tomcatcontainer/start.sh
+++ tomcatcontainer/start.sh
@@ -3,5 +3,5 @@ if [ -f /home/tomcat/tmp/env.properties ] ; then cp /home/tomcat/tmp/env.propert
 cp /home/tomcat/tmp/*.war /opt/tomcat9/webapps
 echo ${MYSQL_PORT} | sed -e 's/tcp:\(\/\/.*\)/db.url=jdbc:mysql:\1\/speaker/g' >> /opt/tomcat9/lib/env.properties
 echo "" >> /opt/tomcat9/lib/env.properties
-export JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/tomcat9/lib/jacoco-agent.jar=destfile=/home/tomcat/tmp/jacocoIT.exec,append=true,includes=com.coveros.*
+export JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/tomcat9/lib/jacoco-agent.jar=destfile=/home/tomcat/tmp/jacoco-it.exec,append=true,includes=com.coveros.*
 /opt/tomcat9/bin/catalina.sh run
